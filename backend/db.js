91955143ef9c898b853e71cef255509cfcb2055b
rissing3-bit <rissing3@cisco.com>
2026-03-02 00:55:08 +0530

    fix: create sqlite data directory at startup

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,9 @@
 const path = require("path");
+const fs = require("fs");
 const Database = require("better-sqlite3");
 
 const dbPath = path.join(__dirname, "data", "todo.sqlite");
+fs.mkdirSync(path.dirname(dbPath), { recursive: true });
 const db = new Database(dbPath);
 
 db.pragma("journal_mode = WAL");
